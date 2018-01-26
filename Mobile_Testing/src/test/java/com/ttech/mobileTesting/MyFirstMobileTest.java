da2fac293f244e482dcd144fe66bafe3436187d6
alamm <alamm@DESKTOP-FLR20HU>
2018-01-26 17:02:39 -0500

    commit by alam

---
+++ Mobile_Testing/src/test/java/com/ttech/mobileTesting/MyFirstMobileTest.java
@@ -0,0 +1,30 @@
+package com.ttech.mobileTesting;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.concurrent.TimeUnit;
+import org.openqa.selenium.By;
+import org.openqa.selenium.remote.BrowserType;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.testng.annotations.Test;
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.AndroidElement;
+import io.appium.java_client.remote.MobileCapabilityType;
+import io.appium.java_client.remote.MobilePlatform;
+
+    public class MyFirstMobileTest {
+    @Test
+	public  void test1() throws MalformedURLException {
+	DesiredCapabilities cap= new DesiredCapabilities();  // this is Selenium package (DesiredCapabilities) class must create object
+	cap.setCapability(MobileCapabilityType.BROWSER_NAME, BrowserType.CHROME); // for browser open formula
+	//cap.setCapability(MobileCapabilityType.PLATFORM,Platform.ANDROID);
+	cap.setCapability(MobileCapabilityType.PLATFORM_NAME, MobilePlatform.ANDROID);
+	cap.setCapability(MobileCapabilityType.DEVICE_NAME, "Android device" );
+	cap.setCapability(MobileCapabilityType.VERSION, "6.0");
+	AndroidDriver<AndroidElement> driver= new AndroidDriver<AndroidElement>(new URL("http://localhost:4723/wd/hub "),cap); // appium connectivity host
+	driver.manage().timeouts().implicitlyWait(30,TimeUnit.SECONDS);
+	driver.get("https://www.facebook.com/");
+	driver.findElement(By.xpath("//*[@id=\"m_login_email\"]")).sendKeys("sjahbd");
+	driver.findElement(By.xpath("//*[@id=\"m_login_password\"]")).sendKeys("1233");
+	driver.findElement(By.xpath("//*[@id=\"u_0_5\"]")).click();
+	} }//End class
---
+++ RestAPI_Project/src/test/java/com/usa/ttech/student/MyFirstTest.java
@@ -0,0 +1,5 @@
+package com.usa.ttech.student;
+
+public class MyFirstTest {
+
+}
